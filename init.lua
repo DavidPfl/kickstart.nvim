b4ec63c60168f42d55a806db60bb77b005682b51
david <@>
2025-04-27 09:43:28 +0200

    Increase surround range

--- init.lua
+++ init.lua
@@ -921,7 +921,7 @@ require('lazy').setup({
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      require('mini.ai').setup { n_lines = 1000 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
